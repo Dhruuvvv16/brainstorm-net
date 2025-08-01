b23340a39608aa9e497c857104e28b00895599ea
Leon287 <dhruvjp04@gmail.com>
2025-08-01 21:47:02 +0530

    Update Neural_Network.py

--- Neural Network from scratch/Neural_Network.py
+++ Neural Network from scratch/Neural_Network.py
@@ -212,3 +212,18 @@ class DeepNN:
             return np.argmax(A, axis=0)
         else:
             return A
+
+#IMPLEMENTATION
+
+from sklearn.datasets import load_breast_cancer
+data = load_breast_cancer()
+X = data.data
+Y = data.target
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+model = DeepNN(X_train,y_train,'binary_crossentropy',alpha=0.01)
+model.train(epochs=1000)
+predictions = model.predict(X_test)
+predictions = np.squeeze(predictions)
+from sklearn.metrics import classification_report
+print(classification_report(y_test,predictions))
